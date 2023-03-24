4264a928978afd271f9df2bfbaf6b5f44d71b8fd
Apsil <tumbler.m.r1@gmail.com>
2023-03-24 11:15:45 +1000

    Added the parser
    Добавлен парсер на PLY.YACC

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import ply.lex as lex
+import ply.yacc as yacc
 
 # Define a list of token names
 tokens = (
+    'NAME',
     'NUMBER',
     'PLUS',
     'MINUS',
@@ -18,13 +20,21 @@ t_TIMES = r'\*'
 t_DIVIDE = r'\/'
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
+t_NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
 
 
 # Define a regular expression for floating point numbers
 def t_NUMBER(t):
-    r'\d+(\.\d+)?'
-    t.value = float(t.value)
+    r'\d+'
+    try:
+        t.value = int(t.value)
+    except ValueError:
+        print("Integer value too large %d", t.value)
+        t.value = 0
     return t
+
+
+t_ignore = " \t"
 
 
 # Define a regular expression for integer numbers
@@ -36,7 +46,7 @@ def t_NUMBER_int(t):
 
 # Define a regular expression for invalid characters
 def t_error(t):
-    #print(f"Invalid character '{t.value[0]}'")
+    print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
 
 
@@ -44,30 +54,70 @@ def t_error(t):
 lexer = lex.lex()
 
 
-# Define a function to parse an arithmetic expression
-def parse_arithmetic_expression(expression):
-    lexer.input(expression)
-    tokens = []
-    while True:
-        tok = lexer.token()
-        if not tok:
-            break
-        tokens.append(tok)
-
-    # Check for division by zero
-    for i in range(len(tokens) - 1):
-        if tokens[i].type == 'DIVIDE' and tokens[i + 1].value == 0:
-            raise ZeroDivisionError("Division by zero")
-
-    # Output tokens in the requested format
-    output = ''
-    for token in tokens:
-        output += f'<LexToken({token.type}, {repr(token.value)}, {token.lineno}, {token.lexpos})>,\n'
-    return output[:-2]  # remove trailing comma and newline
+# Define the grammar rules
+def p_expression_plus(p):
+    'expression : expression PLUS term'
+    p[0] = p[1] + p[3]
 
 
-if __name__ == "__main__":
-    expression = '3.1 + 4 * (2 - 1) / 2'
-    tokens = parse_arithmetic_expression(expression)
-    print(expression, '\n', tokens)
+def p_expression_minus(p):
+    'expression : expression MINUS term'
+    p[0] = p[1] - p[3]
 
+
+def p_expression_term(p):
+    'expression : term'
+    p[0] = p[1]
+
+
+def p_term_times(p):
+    'term : term TIMES factor'
+    p[0] = p[1] * p[3]
+
+
+def p_term_divide(p):
+    'term : term DIVIDE factor'
+    p[0] = p[1] / p[3]
+
+
+def p_term_factor(p):
+    'term : factor'
+    p[0] = p[1]
+
+
+def p_factor_num(p):
+    'factor : NUMBER'
+    p[0] = p[1]
+
+
+def p_factor_expr(p):
+    'factor : LPAREN expression RPAREN'
+    p[0] = p[2]
+
+
+def p_factor_name(p):
+    'factor : NAME'
+    p[0] = 0
+
+
+def p_error(p):
+    if p:
+        print("Syntax error at '%s'" % p.value)
+    else:
+        print("Syntax error at EOF")
+
+
+# Build the parser
+parser = yacc.yacc()
+
+
+# Test the parser
+while True:
+    try:
+        s = input('calc > ')
+    except EOFError:
+        break
+    if not s:
+        continue
+    result = parser.parse(s)
+    print(result)
